ddf6d7f3228816f3ff5f6a11e62c4ef3c59a078c
Süleyman SEZER <hi@iamcodder.com>
2022-08-28 02:14:22 +0300

    Profile image added

--- app/src/main/java/com/patronusstudio/sisecevirmece/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/patronusstudio/sisecevirmece/ui/screens/HomeScreen.kt
@@ -1,20 +1,27 @@
 package com.patronusstudio.sisecevirmece.ui.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.patronusstudio.sisecevirmece.R
@@ -29,11 +36,46 @@ fun HomeScreen() {
             .fillMaxSize()
             .background(BlueViolet)
     ) {
+        UserPic()
+        Username()
         LevelBar()
         HomeCards()
         PlayButton()
     }
 
+}
+
+@Preview
+@Composable
+private fun UserPic() {
+    val context = LocalContext.current
+    val screenWidth = LocalConfiguration.current.screenWidthDp
+    val imageSize = (screenWidth * 0.4).dp
+    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .size(imageSize)
+                .clip(CircleShape)
+                .background(Color.White)
+                .clickable {
+                    Toast
+                        .makeText(context, "Profil resmi", Toast.LENGTH_SHORT)
+                        .show()
+                },
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Image(painter = painterResource(id = R.drawable.player_girl), contentDescription = "")
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun Username(username: String = "Süleyman Sezer") {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Text(text = username, fontSize = 16.sp, style = TextStyle(color = Mustard))
+    }
 }
 
 @Composable
