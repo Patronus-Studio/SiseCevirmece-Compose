4e4a30616c924e35cf907f023f8a55a070e0b7e0
Süleyman SEZER <hi@iamcodder.com>
2022-12-11 21:57:03 +0300

    Package category model updated

--- app/src/main/java/com/patronusstudio/sisecevirmece/data/model/PackageCategoryResponseModel.kt
+++ app/src/main/java/com/patronusstudio/sisecevirmece/data/model/PackageCategoryResponseModel.kt
@@ -1,6 +1,9 @@
 package com.patronusstudio.sisecevirmece.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class PackageCategoryResponseModel(
+    @SerializedName("data")
     val packageCategoryModel: List<PackageCategoryModel>,
     val message: Any,
     val status: String
