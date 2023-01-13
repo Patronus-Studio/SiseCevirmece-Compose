42d94a9cf9f968d86f498902de643de1da9f4c5c
Süleyman SEZER <hi@iamcodder.com>
2023-01-13 10:17:47 +0300

    When bottle spin a rotate, again clickable issue solved

--- app/src/main/java/com/patronusstudio/sisecevirmece/ui/screens/NormalGameScreen.kt
+++ app/src/main/java/com/patronusstudio/sisecevirmece/ui/screens/NormalGameScreen.kt
@@ -25,26 +25,31 @@ import com.patronusstudio.sisecevirmece.ui.theme.AppColor
 import com.patronusstudio.sisecevirmece.ui.widgets.CardTitle
 import kotlin.random.Random
 
+private enum class TouchListener {
+    INIT,
+    ANIM_STARTED,
+    ANIM_ENDED
+}
+
 @Composable
 fun NormalGameScreen(backClicked: () -> Unit) {
     val screenWidth = LocalConfiguration.current.screenWidthDp
     val bottleSize = (screenWidth * 0.9).dp
-    val degree = remember {
-        mutableStateOf(0f)
-    }
+    val degree = remember { mutableStateOf(0f) }
     val bottleRotationValue = 10f
     var spinTimer = 0
-    val touchStatus = remember {
-        mutableStateOf(TouchListener.INIT)
-    }
+    val touchStatus = remember { mutableStateOf(TouchListener.INIT) }
+    val isSpinning = remember { mutableStateOf(false) }
     val animFinished = {
         degree.value = degree.value % 360
         touchStatus.value = TouchListener.ANIM_ENDED
+        isSpinning.value = false
     }
     val infiniteAnim = rememberUpdatedState(newValue = animateFloatAsState(
         targetValue = degree.value,
         animationSpec = tween(durationMillis = 5000), finishedListener = { animFinished() }
     ))
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -65,11 +70,13 @@ fun NormalGameScreen(backClicked: () -> Unit) {
                     )
                     .pointerInput(Unit) {
                         this.detectTransformGestures { centroid, pan, zoom, rotation ->
-                            if (pan.x > 0 && pan.y > 0) degree.value += bottleRotationValue
-                            else if (pan.x < 0 && pan.y < 0) degree.value -= bottleRotationValue
-                            else if (pan.x > 0 && pan.y < 0) degree.value -= bottleRotationValue
-                            else degree.value += bottleRotationValue
-                            spinTimer++
+                            if (isSpinning.value.not()) {
+                                if (pan.x > 0 && pan.y > 0) degree.value += bottleRotationValue
+                                else if (pan.x < 0 && pan.y < 0) degree.value -= bottleRotationValue
+                                else if (pan.x > 0 && pan.y < 0) degree.value -= bottleRotationValue
+                                else degree.value += bottleRotationValue
+                                spinTimer++
+                            }
                         }
                     }
                     .pointerInput(Unit) {
@@ -80,11 +87,15 @@ fun NormalGameScreen(backClicked: () -> Unit) {
                                 val canceled =
                                     event.changes.any { it.isConsumed && it.positionChanged() }
                             } while (!canceled && event.changes.any { it.pressed })
-                            touchStatus.value = TouchListener.ANIM_STARTED
-                            val result = degree.value % 100
-                            degree.value = if (result < 50)
-                                (result * (Random.nextInt(50, 75)) + spinTimer)
-                            else (result * (Random.nextInt(20, 35)) + spinTimer)
+                            if (isSpinning.value.not()) {
+                                touchStatus.value = TouchListener.ANIM_STARTED
+                                val result = degree.value % 100
+                                degree.value = if (result < 50)
+                                    (result * (Random.nextInt(50, 75)) + spinTimer)
+                                else (result * (Random.nextInt(20, 35)) + spinTimer)
+                                spinTimer = 0
+                                isSpinning.value = true
+                            }
                         }
                     }
             )
@@ -92,8 +103,3 @@ fun NormalGameScreen(backClicked: () -> Unit) {
     }
 }
 
-private enum class TouchListener {
-    INIT,
-    ANIM_STARTED,
-    ANIM_ENDED
-}
