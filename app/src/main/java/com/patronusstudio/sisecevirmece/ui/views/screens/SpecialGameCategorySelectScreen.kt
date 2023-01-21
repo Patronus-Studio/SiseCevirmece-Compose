523983e300907cee0906d5bd3e939f6d7ab2aa33
Süleyman SEZER <hi@iamcodder.com>
2023-01-21 23:57:35 +0300

    Package doesnt find animation added

--- app/src/main/java/com/patronusstudio/sisecevirmece/ui/views/screens/SpecialGameCategorySelectScreen.kt
+++ app/src/main/java/com/patronusstudio/sisecevirmece/ui/views/screens/SpecialGameCategorySelectScreen.kt
@@ -3,13 +3,26 @@ package com.patronusstudio.sisecevirmece.ui.views.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.patronusstudio.sisecevirmece.R
 import com.patronusstudio.sisecevirmece.data.model.dbmodel.PackageDbModel
 import com.patronusstudio.sisecevirmece.data.viewModels.SpecialGameCategorySelectViewModel
@@ -18,14 +31,16 @@ import com.patronusstudio.sisecevirmece.ui.widgets.BaseBackground
 import com.patronusstudio.sisecevirmece.ui.widgets.CardImageWithText
 
 //hiç oynanacak paketi yoksa uyarı bas ekranda
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SpecialGameCategorySelectScreen(backClicked: () -> Unit, passGameScreen: () -> Unit) {
     val viewModel = hiltViewModel<SpecialGameCategorySelectViewModel>()
     val cardWidth = (LocalConfiguration.current.screenWidthDp * 0.3).dp
     val cardHeight = (LocalConfiguration.current.screenHeightDp * 0.20).dp
     val imageSize = (LocalConfiguration.current.screenHeightDp * 0.12).dp
-    val spaceSizeCards = ((LocalConfiguration.current.screenWidthDp.dp) - (cardWidth *2))/3
-
+    val spaceSizeCards = ((LocalConfiguration.current.screenWidthDp.dp) - (cardWidth * 2)) / 3
+    val emptyPackageImageSize = (LocalConfiguration.current.screenWidthDp * 0.75).dp
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.package_empty))
     LaunchedEffect(key1 = Unit, block = {
         viewModel.getAllPackages()
     })
@@ -46,7 +61,7 @@ fun SpecialGameCategorySelectScreen(backClicked: () -> Unit, passGameScreen: () 
                     } else {
                         SingeCard(model = model, clicked = {
                             viewModel.setShowStatu(model)
-                        }, cardWidth, cardHeight, imageSize,spaceSizeCards)
+                        }, cardWidth, cardHeight, imageSize, spaceSizeCards)
                     }
                 } else if (index + 2 <= viewModel.packages.size - 1) {
                     DoubleCard(
@@ -61,10 +76,36 @@ fun SpecialGameCategorySelectScreen(backClicked: () -> Unit, passGameScreen: () 
                 } else if (index + 1 <= viewModel.packages.size - 1) {
                     SingeCard(model = viewModel.packages[index + 1], clicked = {
                         viewModel.setShowStatu(viewModel.packages[index + 1])
-                    }, cardWidth, cardHeight, imageSize,spaceSizeCards)
+                    }, cardWidth, cardHeight, imageSize, spaceSizeCards)
                 }
             }
         })
+        if (viewModel.packages.size == 0) {
+            Dialog(
+                onDismissRequest = {
+                    backClicked()
+                },
+                properties = DialogProperties(usePlatformDefaultWidth = false)
+            ) {
+                Column(
+                    Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    LottieAnimation(
+                        composition = composition,
+                        iterations = LottieConstants.IterateForever,
+                        modifier = Modifier.size(emptyPackageImageSize),
+                    )
+                    Text(
+                        text = stringResource(R.string.paket_doesnt_find_to_play),
+                        color = AppColor.White,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Normal,
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -75,7 +116,7 @@ private fun SingeCard(
     cardWidth: Dp,
     cardHeight: Dp,
     imageSize: Dp,
-    spaceSize:Dp
+    spaceSize: Dp
 ) {
     Spacer(modifier = Modifier.height(16.dp))
     Row(Modifier.fillMaxWidth()) {
@@ -84,11 +125,11 @@ private fun SingeCard(
             image = model.packageImage!!,
             text = model.packageName,
             backgroundColor = if (model.isSelected.not()) AppColor.White else AppColor.ScreaminGreen,
-            textColor = if(model.isSelected) AppColor.DavysGrey else AppColor.DavysGrey,
+            textColor = if (model.isSelected) AppColor.DavysGrey else AppColor.DavysGrey,
             cardSizeWidth = cardWidth,
             cardSizeHeight = cardHeight,
             imageSize = imageSize,
-            borderColor =  if(model.isSelected) AppColor.White else AppColor.SeaSerpent,
+            borderColor = if (model.isSelected) AppColor.White else AppColor.SeaSerpent,
             clicked = clicked
         )
     }
@@ -107,22 +148,22 @@ private fun DoubleCard(
             image = firstModel.packageImage!!,
             text = firstModel.packageName,
             backgroundColor = if (firstModel.isSelected.not()) AppColor.White else AppColor.ScreaminGreen,
-            textColor = if(firstModel.isSelected) AppColor.DavysGrey else AppColor.DavysGrey,
+            textColor = if (firstModel.isSelected) AppColor.DavysGrey else AppColor.DavysGrey,
             cardSizeWidth = cardWidth,
             cardSizeHeight = cardHeight,
             imageSize = imageSize,
-            borderColor = if(firstModel.isSelected) AppColor.White else AppColor.SeaSerpent,
+            borderColor = if (firstModel.isSelected) AppColor.White else AppColor.SeaSerpent,
             clicked = firstClicked
         )
         CardImageWithText(
             image = secondModel.packageImage!!,
             text = secondModel.packageName,
             backgroundColor = if (secondModel.isSelected.not()) AppColor.White else AppColor.ScreaminGreen,
-            textColor = if(secondModel.isSelected) AppColor.DavysGrey else AppColor.DavysGrey,
+            textColor = if (secondModel.isSelected) AppColor.DavysGrey else AppColor.DavysGrey,
             cardSizeWidth = cardWidth,
             cardSizeHeight = cardHeight,
             imageSize = imageSize,
-            borderColor = if(secondModel.isSelected) AppColor.White else AppColor.SeaSerpent,
+            borderColor = if (secondModel.isSelected) AppColor.White else AppColor.SeaSerpent,
             clicked = secondClicked
         )
 
