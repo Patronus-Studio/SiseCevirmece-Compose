60288a08396dcb8785fc5d5e8eba5a0f26f233f2
Süleyman SEZER <hi@iamcodder.com>
2023-01-25 00:36:06 +0300

    Truth dare package id fetch issue solved

--- app/src/main/java/com/patronusstudio/sisecevirmece/data/viewModels/SpecialGameScreenViewModel.kt
+++ app/src/main/java/com/patronusstudio/sisecevirmece/data/viewModels/SpecialGameScreenViewModel.kt
@@ -6,12 +6,15 @@ import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.patronusstudio.sisecevirmece.data.enums.BottleTouchListener
+import com.patronusstudio.sisecevirmece.data.enums.TruthDareDefaultPackageEnum
 import com.patronusstudio.sisecevirmece.data.model.dbmodel.PackageDbModel
 import com.patronusstudio.sisecevirmece.data.model.dbmodel.QuestionDbModel
 import com.patronusstudio.sisecevirmece.data.repository.local.PackageLocalRepository
 import com.patronusstudio.sisecevirmece.data.repository.local.QuestionLocalRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.withContext
@@ -34,8 +37,8 @@ class SpecialGameScreenViewModel @Inject constructor(
     val selectedLists: StateFlow<List<PackageDbModel>?> get() = _selectedLists
 
     private val _packagesAndQuestions =
-        MutableStateFlow<HashMap<PackageDbModel, List<QuestionDbModel>>>(hashMapOf())
-    val packagesAndQuestions: StateFlow<HashMap<PackageDbModel, List<QuestionDbModel>>> get() = _packagesAndQuestions
+        MutableStateFlow<HashMap<Int, List<QuestionDbModel>>>(hashMapOf())
+    val packagesAndQuestions: StateFlow<HashMap<Int, List<QuestionDbModel>>> get() = _packagesAndQuestions
 
     private val _randomPackage = MutableStateFlow<PackageDbModel?>(null)
     val randomPackage: StateFlow<PackageDbModel?> get() = _randomPackage
@@ -44,15 +47,23 @@ class SpecialGameScreenViewModel @Inject constructor(
     val randomQuestion: StateFlow<QuestionDbModel?> get() = _randomQuestion
 
     suspend fun jsonToModel(jsonModel: String) {
+        _packagesAndQuestions.value = hashMapOf()
+
         val listType = object : TypeToken<ArrayList<PackageDbModel?>?>() {}.type
         val packages: List<PackageDbModel> = Gson().fromJson(jsonModel, listType)
         _selectedLists.value = packages
-        withContext(Dispatchers.IO) {
-            packages.forEach {
-                getQuestions(it)
-            }
-            getRandomPackage()
-            getRandomQuestion()
+        coroutineScope {
+            async(Dispatchers.IO) {
+                packages.forEach {
+                    getQuestions(it)
+                }
+            }.await()
+            async {
+                getRandomPackage()
+            }.await()
+            async {
+                getRandomQuestion()
+            }.await()
         }
     }
 
@@ -62,14 +73,28 @@ class SpecialGameScreenViewModel @Inject constructor(
 
 
     private suspend fun getQuestions(packageDbModel: PackageDbModel) {
-        val questions = questionLocalRepository.getQuestionsWithPackageId(packageDbModel.primaryId)
+        val packageId = when (packageDbModel.cloudPackageCategoryId) {
+            TruthDareDefaultPackageEnum.TRUTH.getPackageCategoryId() -> {
+                TruthDareDefaultPackageEnum.TRUTH.getPackageCategoryId()
+            }
+            TruthDareDefaultPackageEnum.DARE.getPackageCategoryId() -> {
+                TruthDareDefaultPackageEnum.DARE.getPackageCategoryId()
+            }
+            else -> packageDbModel.primaryId
+        }
+        val questions = questionLocalRepository.getQuestionsWithPackageId(packageId)
         val filteredQuestions = questions.filter { it.isShowed.not() }
         if (filteredQuestions.isEmpty()) {
-            questionLocalRepository.updateQuestionShowStatu(false, packageDbModel.primaryId)
+            withContext(Dispatchers.IO) {
+                questionLocalRepository.updateAllQuestionsShowStatus(
+                    packageDbModel.primaryId, false
+                )
+            }
             questions.forEach { it.isShowed = false }
-            _packagesAndQuestions.value[packageDbModel] = questions
+            Log.d("Sülo", "getQuestions: ${packageDbModel.packageName} ${questions.size}")
         } else {
-            _packagesAndQuestions.value[packageDbModel] = filteredQuestions
+            _packagesAndQuestions.value[packageDbModel.primaryId] = filteredQuestions
+            Log.d("Sülo", "getQuestions: ${packageDbModel.packageName} ${filteredQuestions.size}")
         }
     }
 
@@ -79,22 +104,20 @@ class SpecialGameScreenViewModel @Inject constructor(
 
     suspend fun getRandomQuestion() {
         _isLoading.value = true
-        val randomQst = _packagesAndQuestions.value[_randomPackage.value]?.random()
+        val randomQst = _packagesAndQuestions.value[_randomPackage.value!!.primaryId]?.random()
         if (randomQst == null) {
             questionLocalRepository.updateQuestionShowStatu(false, _randomPackage.value!!.primaryId)
             val fetchedQuestions =
                 questionLocalRepository.getQuestionsWithPackageId(_randomPackage.value!!.primaryId)
-            _packagesAndQuestions.value[_randomPackage.value!!] = fetchedQuestions
+            _packagesAndQuestions.value[_randomPackage.value!!.primaryId] = fetchedQuestions
             getRandomQuestion()
         } else {
             _randomQuestion.value = randomQst
             questionLocalRepository.updateQuestionShowStatu(true, randomQuestion.value!!.primaryId)
-            val listOnMap = packagesAndQuestions.value[_randomPackage.value]
+            val listOnMap = packagesAndQuestions.value[_randomPackage.value!!.primaryId]
             listOnMap!!.toMutableList().remove(randomQuestion.value)
-            _packagesAndQuestions.value[_randomPackage.value!!] = listOnMap
+            _packagesAndQuestions.value[_randomPackage.value!!.primaryId] = listOnMap
         }
-        Log.d("Süle_randomPackage:", "${_randomPackage.value} ")
-        Log.d("Süle _randomQuestion:", "${_randomQuestion.value} ")
         _isLoading.value = false
     }
 
