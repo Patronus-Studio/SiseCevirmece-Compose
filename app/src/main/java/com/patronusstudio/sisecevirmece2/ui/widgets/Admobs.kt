30681192fca9d1905b00d8b3f6099e3b405a1a74
Süleyman SEZER <hi@iamcodder.com>
2023-07-08 02:40:21 +0300

    CreateInterstitialAd functionality added

--- app/src/main/java/com/patronusstudio/sisecevirmece2/ui/widgets/Admobs.kt
+++ app/src/main/java/com/patronusstudio/sisecevirmece2/ui/widgets/Admobs.kt
@@ -1,15 +1,19 @@
 package com.patronusstudio.sisecevirmece2.ui.widgets
 
 import android.app.Activity
-import android.content.Context
+import android.os.Handler
 import android.view.Gravity
 import android.widget.FrameLayout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.applovin.mediation.MaxAd
 import com.applovin.mediation.MaxAdFormat
+import com.applovin.mediation.MaxAdListener
+import com.applovin.mediation.MaxError
 import com.applovin.mediation.ads.MaxAdView
+import com.applovin.mediation.ads.MaxInterstitialAd
 import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.FullScreenContentCallback
@@ -17,6 +21,9 @@ import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.patronusstudio.sisecevirmece2.data.enums.InterstitialAdViewLoadStatusEnum
+import com.patronusstudio.sisecevirmece2.data.utils.getActivity
+import java.util.concurrent.TimeUnit
+import kotlin.math.pow
 
 // TODO: banner da sorun olabilir eski android viewine bir bak
 @Composable
@@ -24,6 +31,7 @@ fun BannerAdView(adUnitId: String) {
     AndroidView(
         modifier = Modifier.fillMaxSize(),
         factory = { context ->
+            context.getActivity()
             val bannerView = MaxAdView(adUnitId, MaxAdFormat.BANNER, context).apply {
                 this.loadAd()
                 this.gravity = Gravity.BOTTOM
@@ -86,6 +94,80 @@ object InterstitialAdView {
     fun removeInterstitial() {
         mInterstitialAd?.fullScreenContentCallback = null
         mInterstitialAd = null
+    }
+}
+
+object ApplovinUtils {
+
+    private lateinit var interstitialAd: MaxInterstitialAd
+
+    @Composable
+    fun CreateInterstitialAd(
+        adUnitId: String,
+        onAdShowed: (() -> Unit)? = null,
+        onAdClosed: (() -> Unit)? = null
+    ) {
+        var retryAttempt = 0.0
+
+        AndroidView(factory = { context ->
+            if (this::interstitialAd.isInitialized.not()) {
+                interstitialAd = MaxInterstitialAd(adUnitId, context.getActivity())
+            } else {
+                if (interstitialAd.adUnitId != adUnitId) {
+                    interstitialAd = MaxInterstitialAd(adUnitId, context.getActivity())
+                }
+            }
+            FrameLayout(context).also {
+                interstitialAd.setListener(object : MaxAdListener {
+                    override fun onAdLoaded(p0: MaxAd?) {
+                        // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
+                        // Reset retry attempt
+                        retryAttempt = 0.0
+                        if (interstitialAd.isReady) {
+                            interstitialAd.showAd()
+                        } else {
+                            interstitialAd.loadAd()
+                        }
+
+                    }
+
+                    override fun onAdDisplayed(p0: MaxAd?) {
+                        onAdShowed?.let {
+                            onAdShowed()
+                        }
+                    }
+
+                    override fun onAdHidden(p0: MaxAd?) {
+                        // Interstitial ad is hidden. Pre-load the next ad
+                        onAdClosed?.let {
+                            onAdClosed()
+                        }
+                    }
+
+                    override fun onAdClicked(p0: MaxAd?) {
+                    }
+
+                    override fun onAdLoadFailed(p0: String?, p1: MaxError?) {
+                        // Interstitial ad failed to load
+                        // AppLovin recommends that you retry with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+                        retryAttempt++
+                        val delayMillis =
+                            TimeUnit.SECONDS.toMillis(
+                                2.0.pow(6.0.coerceAtMost(retryAttempt)).toLong()
+                            )
+                        Handler().postDelayed({ interstitialAd.loadAd() }, delayMillis)
+                    }
+
+                    override fun onAdDisplayFailed(p0: MaxAd?, p1: MaxError?) {
+                        // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
+                        interstitialAd.loadAd()
+                    }
+
+                })
+                // Load the first ad
+                interstitialAd.loadAd()
+            }
+        })
     }
 }
 
